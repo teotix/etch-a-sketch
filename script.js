895223974f37c3e09a79134af199e860372c3dcb
teotix <teo_kostic@yahoo.com>
2023-12-15 20:02:41 +0100

    Finish sketching logic and grid erase and create

--- script.js
+++ script.js
@@ -1,42 +1,88 @@
 const sketchContainer = document.querySelector(".sketch-container");
+const buttons = document.querySelectorAll("button");
+const slider = document.querySelector("#grid-slider");
+const btn1 = document.querySelector("#btn1");
+const btn2 = document.querySelector("#btn2");
+const sliderNumber = document.querySelector('#slider-num')
+
+let holding = false;
+
+addEventListener("dragstart", function (e) {
+  e.preventDefault();
+});
+
+function clicked(e) {
+  buttons.forEach((button) => {
+    button.classList.remove("clicked");
+  });
+  e.target.classList.toggle("clicked");
+}
 
 function changeColor(e) {
   e.target.style.backgroundColor = "black";
 }
 
-function gridCreator(gridSize) {
-  for (let i = 0; i < gridSize; i++) {
-    const createdDivRow = document.createElement("div");
-    createdDivRow.style.outline = "1px solid red";
-    createdDivRow.style.width = "100%";
-    createdDivRow.style.height = "100%";
-    createdDivRow.style.display = "flex";
-    createdDivRow.style.flex = "content";
-    for (let j = 0; j < gridSize; j++) {
-      const createdDiv = document.createElement("div");
-      createdDiv.style.outline = "1px solid green";
-      createdDiv.style.display = "flex";
-      createdDiv.style.flex = "content";
+function gridCreator() {
+  sliderNumber.textContent = `${slider.value}x${slider.value}`
+  for (let i = 0; i < slider.value; i++) {
+    let createdDivRow = document.createElement("div");
+    createdDivRow.style.cssText =
+      "width: 100%; height: 100%; display: flex; flex: content;";
+    for (let j = 1; j < slider.value; j++) {
+      let createdDiv = document.createElement("div");
+      sketchContainer.addEventListener("mousedown", (e) => {
+        holding = true;
+      });
+      sketchContainer.addEventListener("mouseup", (e) => {
+        holding = false;
+      });
+      sketchContainer.addEventListener("mouseleave", (e) => {
+        holding = false;
+      });
+      sketchContainer.addEventListener("mousemove", (e) => {
+        if (holding == true) {
+          e.target.style.backgroundColor = "black";
+        }
+      });
+      createdDiv.style.cssText = "display: flex; flex: content;";
       createdDivRow.appendChild(createdDiv);
     }
     sketchContainer.appendChild(createdDivRow);
   }
 }
 
-for (let i = 0; i < 50; i++) {
-  const createdDivRow = document.createElement("div");
-  createdDivRow.setAttribute("draggable", false);
-  createdDivRow.style.cssText =
-    "width: 100%; height: 100%; display: flex; flex: content;";
-  for (let j = 0; j < 50; j++) {
-    const createdDiv = document.createElement("div");
-    // createdDiv.setAttribute("draggable", false);
-    // createdDiv.className = 'asdf'
-    createdDiv.addEventListener("dragover", (e) => {
-      e.target.style.backgroundColor = "black";
-    });
-    createdDiv.style.cssText = "display: flex; flex: content;";
-    createdDivRow.appendChild(createdDiv);
-  }
-  sketchContainer.appendChild(createdDivRow);
-}
+buttons.forEach((button) => {
+  button.addEventListener("click", clicked);
+});
+slider.addEventListener('change', (e) => {
+  sliderNumber.textContent = `${e.target.value}x${e.target.value}`
+})
+slider.addEventListener("change", (e) => {
+  sketchContainer.innerHTML = "";
+});
+slider.addEventListener("change", gridCreator);
+
+gridCreator();
+
+// for (let i = 0; i < 50; i++) {
+//   const createdDivRow = document.createElement("div");
+//   createdDivRow.style.cssText =
+//     "width: 100%; height: 100%; display: flex; flex: content;";
+//   for (let j = 0; j < 50; j++) {
+//     const createdDiv = document.createElement("div");
+//     sketchContainer.addEventListener("mousedown", (e) => {
+//       holding = true;
+//     });
+//     sketchContainer.addEventListener("mouseup", (e) => {
+//       holding = false;
+//     });
+//     sketchContainer.addEventListener("mousemove", (e) => {
+//       if (holding == true) {
+//         e.target.style.backgroundColor = "black";
+//       }
+//     });
+//     createdDiv.style.cssText = "display: flex; flex: content;";
+//     createdDivRow.appendChild(createdDiv);
+//   }
+//   sketchContainer.appendChild(createdDivRow);
+// }
